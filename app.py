1865b3768b24cf1cd1e541462873dc0d5762c3d9
jiya6782 <gaurikothari24@gmail.com>
2026-01-09 20:57:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,21 +11,7 @@ import helper.py
 
 LOCAL_TZ = pytz.timezone("America/New_York")
 
-# Initialize the study list in session_state
-if "study_list" not in st.session_state:
-    if os.path.exists("tasks.json"):
-        with open("tasks.json", mode="r") as file:
-            data = json.load(file)
-            st.session_state.study_list = data.get("study_list")
-            st.session_state.user_name = data.get("user_name")
-            
-    else:
-        st.session_state.study_list = []
-        st.session_state.user_name = ""
-    
-for task in st.session_state.study_list:
-    if "reminded" not in task:
-        task["reminded"] = False
+
 
 
 
@@ -281,5 +267,6 @@ elif option == "Edit Assignment":
                 st.success("Email updated!")
             else:
                 st.info("Email unchanged")
+
 
 
